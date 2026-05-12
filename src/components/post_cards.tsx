63accc99cc5f5a1bb6909750ca3ecc87647d01df
ElktrumElk <festinacole373@gmail.com>
2026-05-12 13:14:49 +0000

    Update 0.1.7

--- src/components/post_cards.tsx
+++ src/components/post_cards.tsx
@@ -12,8 +12,8 @@ export default function PostCards() {
 
                         <div style={{ width: '100%', padding: '.4rem', display: 'flex', justifyContent: 'space-between' }}>
 
-                            <div style={{display: 'flex', gap: '.3rem'}}>
-                                <div style={{background: '#03344b', width: '40px', height: '40px', color: 'white', padding: '20px', borderRadius: '100px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                            <div style={{ display: 'flex', gap: '.3rem' }}>
+                                <div style={{ background: '#03344b', width: '40px', height: '40px', color: 'white', padding: '20px', borderRadius: '100px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                                     <span>{dat.username.substring(0, 1)}</span>
                                 </div>
 
@@ -25,19 +25,34 @@ export default function PostCards() {
                             </div>
                         </div>
 
-                        <div style={{width: '100%', height: '200px', overflow: 'hidden', background: 'black', borderRadius: '1.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', objectFit: 'cover'}}>
-                            <img src={dat.img} style={{width: 'inherit'}}/>
+                        <div style={{ width: '100%', height: '250px', overflow: 'hidden', background: 'black', borderRadius: '1.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', objectFit: 'cover' }}>
+                            <img src={dat.img} style={{ width: 'inherit' }} />
                         </div>
 
-                        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <strong>{dat.category.toUpperCase()}</strong>
-                            <span style={{color: 'gray'}}>{dat.time_posted}</span>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <strong style={{ fontSize: '.9rem', lineHeight: '.9rem', color: '#033863' }}>{dat.category.toUpperCase()}</strong>
+                            <span style={{ color: 'gray', fontSize: '.8rem', lineHeight: '.8rem' }}>{dat.time_posted}</span>
                         </div>
 
-                        <article style={{width: '100%', display: 'flex', flexDirection: 'column', gap: '.5rem'}}>
-                            <h3>{dat.title}</h3>
-                            <p style={{color: '#5c5c5c'}}>{dat.description}</p>
+                        <article style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '.5rem' }}>
+                            <h2>{dat.title}</h2>
+                            <p style={{ color: '#5c5c5c' }}>{dat.description}</p>
                         </article>
+
+                        <div style={{width: '100%', display: 'flex', gap: '.3rem', alignItems: 'center'}}>
+                            <div>
+                                <img alt="ratings" />
+                                <span>{dat.ratings}</span>
+                            </div>
+                            <div>
+                                <img alt="comment" />
+                                <span>{dat.comment}</span>
+                            </div>
+                             <div>
+                                <img alt="share"/>
+                                <span>{dat.share}</span>
+                            </div>
+                        </div>
                     </div>
                 ))
             }
--- src/data/mockData.tsx
+++ src/data/mockData.tsx
@@ -1,107 +1,157 @@
 
 export const postsData = [
-    { 
-        id: 1, 
-        username: 'ElktrumElk', 
-        category: 'Portfolio', 
+    {
+        id: 1,
+        username: 'ElktrumElk',
+        category: 'Portfolio',
         img: 'https://cdn.dribbble.com/userupload/25262631/file/original-dfe419a034fee3bb345f6c08def7e764.jpg?resize=1024x768&vertical=center',
         time_posted: '2 days ago',
         postId: '01',
         description: 'Just finished the redesign for a FinTech dashboard. Focused on high-contrast data viz!',
-        title: 'FinTech Dahsboard' 
+        title: 'FinTech Dahsboard',
+        ratings: 10000,
+        views: 50000,
+        comment: 250,
+        preview: 20000,
+        share: 100
+
     },
-    { 
-        id: 2, 
-        username: 'Sheik', 
+    {
+        id: 2,
+        username: 'Sheik',
         category: 'Code Snippets',
         time_posted: '1 days ago',
         img: 'https://cdn.dribbble.com/userupload/47335980/file/1914640f00a37c812f4098147b5beabf.jpg?format=webp&resize=1000x750&vertical=center',
         postId: '02',
-        description: 'A clean way to handle multi-step forms in React using a custom useReducer hook. 🚀' ,
-        title: 'Multi-Step Forms' 
+        description: 'A clean way to handle multi-step forms in React using a custom useReducer hook. 🚀',
+        title: 'Multi-Step Forms',
+        ratings: 10000,
+        views: 50000,
+        comment: 250,
+        preview: 20000,
+        share: 100
 
     },
-    { 
-        id: 3, 
-        username: 'Aziz', 
-        category: 'UI/UX Design', 
+    {
+        id: 3,
+        username: 'Aziz',
+        category: 'UI/UX Design',
         img: 'https://cdn.dribbble.com/userupload/45818243/file/8e9b1f3001000849bd0c8dabff5dbe69.png?format=webp&resize=1000x750&vertical=center',
         postId: '03',
         time_posted: '3 weeks ago',
-        description: 'Exploring Neumorphism vs. Glassmorphism for a music player app. Which one do you prefer?' ,
-        title: 'Music Player app' 
+        description: 'Exploring Neumorphism vs. Glassmorphism for a music player app. Which one do you prefer?',
+        title: 'Music Player app',
+        ratings: 10000,
+        views: 50000,
+        comment: 250,
+        preview: 20000,
+        share: 100
 
     },
-    { 
-        id: 4, 
-        username: 'Favmaclegend', 
-        category: 'Micro-Tutorials', 
+    {
+        id: 4,
+        username: 'Favmaclegend',
+        category: 'Micro-Tutorials',
         img: 'https://cdn.dribbble.com/userupload/47676718/file/23c6b132af44e11dd38c3b63433d5b45.png?format=webp&resize=800x600&vertical=center',
         postId: '04',
         time_posted: '2 days ago',
-        description: 'Quick tip: Use the "inset" CSS property to replace top/right/bottom/left in one line!' ,
-        title: 'Quick CSS Tip' 
+        description: 'Quick tip: Use the "inset" CSS property to replace top/right/bottom/left in one line!',
+        title: 'Quick CSS Tip',
+        ratings: 3000,
+        views: 8000,
+        comment: 20,
+        preview: 2000,
+        share: 100
     },
-    { 
-        id: 5, 
-        username: 'AG', 
-        category: 'Tech Stack', 
+    {
+        id: 5,
+        username: 'AG',
+        category: 'Tech Stack',
         img: 'https://unsplash.com',
         time_posted: '1hr ago',
         postId: '05',
         description: 'My 2024 VS Code setup: Gruvbox theme, Cascadia Code font, and 15 essential extensions.',
-        title: 'Vs Code Setup' 
-
+        title: 'Vs Code Setup',
+        ratings: 300,
+        views: 1000,
+        comment: 60,
+        preview: 80,
+        share: 6
     },
-    { 
-        id: 6, 
-        username: 'Dev_Sarah', 
-        category: 'Performance', 
+    {
+        id: 6,
+        username: 'Dev_Sarah',
+        category: 'Performance',
         img: 'https://unsplash.com',
         time_posted: '12 hr ago',
         postId: '06',
-        description: 'Managed to slash our bundle size by 40% using dynamic imports and image optimization.' ,
-        title: 'Using Dynamic Imports' 
+        description: 'Managed to slash our bundle size by 40% using dynamic imports and image optimization.',
+        title: 'Using Dynamic Imports',
+        ratings: 8000,
+        views: 10000,
+        comment: 60,
+        preview: 1000,
+        share: 28
     },
-    { 
-        id: 7, 
-        username: 'CodeNinja', 
-        category: 'Educational & Tips', 
+    {
+        id: 7,
+        username: 'CodeNinja',
+        category: 'Educational & Tips',
         img: 'https://unsplash.com',
         postId: '07',
         time_posted: '4 days ago',
         description: 'Why you should stop using px for font sizes and switch to rem for better accessibility.',
-        title: 'Why You SHould Stop Using PX for Font Size' 
+        title: 'Why You SHould Stop Using PX for Font Size',
+        ratings: 700,
+        views: 10000,
+        comment: 750,
+        preview: 800,
+        share: 6
     },
-    { 
-        id: 8, 
-        username: 'Sheik', 
-        category: 'Behind the Scenes', 
+    {
+        id: 8,
+        username: 'Sheik',
+        category: 'Behind the Scenes',
         img: 'https://unsplash.com',
         time_posted: '2 days ago',
         postId: '08',
         description: 'Late night debugging session. Finally squashed that race condition in the auth flow!',
-        title: 'Late night Debugging' 
+        title: 'Late night Debugging',
+        ratings: 300,
+        views: 1000,
+        comment: 60,
+        preview: 80,
+        share: 6
     },
-    { 
-        id: 9, 
-        username: 'Aziz', 
-        category: 'Portfolio', 
+    {
+        id: 9,
+        username: 'Aziz',
+        category: 'Portfolio',
         img: 'https://unsplash.com',
         time_posted: '3 hr ago',
         postId: '09',
         description: 'Check out this landing page for a sustainable coffee brand. Minimalist and fast.',
-        title: 'Landing Page' 
+        title: 'Landing Page',
+        ratings: 300,
+        views: 1000,
+        comment: 89,
+        preview: 80,
+        share: 6
     },
-    { 
-        id: 10, 
-        username: 'ElktrumElk', 
-        category: 'Resources', 
+    {
+        id: 10,
+        username: 'ElktrumElk',
+        category: 'Resources',
         img: 'https://unsplash.com',
         time_posted: '2 days ago',
         postId: '010',
         description: 'Free resource: I curated a list of 50+ high-quality SVGs for your next landing page.',
-        title: '50+ High Quality' 
+        title: '50+ High Quality',
+        ratings: 180,
+        views: 270,
+        comment: 35,
+        preview: 96,
+        share: 60
 
     }
 ];
