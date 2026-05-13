4bb0547bd867d0296435a22bbbfdf7990600d59c
ElktrumElk <festinacole373@gmail.com>
2026-05-13 13:38:05 +0000

    b update 0.2

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -51,13 +51,32 @@ export default function AddPost() {
         }, 300)
     }
 
+    const [tag, setTag] = useState([]);
+    const [addTagPanel, setAddTagPanel] = useState(false);
+
+    const handleTag = (tagName: string) => {
+        if (tag.includes(tagName)) { setTag(prev => prev.filter((items => items !== tagName))); return }
+        setTag(prev => [...prev, tagName]);
+    }
+
+    const handTagPanel = (state: boolean, e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setAddTagPanel(state);
+    }
+
+    const tagInputElement = useRef<HTMLInputElement>(null);
+
+    const handleAddTag = () => {
+        handleTag(tagInputElement.current?.value.length !== 0 ? tagInputElement.current?.value : 'unknown');
+    }
+
     return (
         <div style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem', padding: '1rem' }}>
             <form style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem' }}>
 
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '.3rem', width: '100%' }}>
                     <label htmlFor="title" style={{ fontWeight: 'bold' }}>Title</label>
-                    <input id="title" placeholder="Title: Responsive Web design" required style={{ width: '100%', padding: '.4rem', outline: 'none', border: 'none', borderRadius: '.5rem', fontSize: '1rem' }} />
+                    <input id="title" placeholder="Title: Responsive Web design" required style={{ width: '100%', padding: '1rem', outline: 'none', border: 'none', borderRadius: '.5rem', fontSize: '1rem' }} />
                 </div>
 
                 <div style={{ width: '100%', background: 'white', borderRadius: '1rem', display: 'flex', flexDirection: 'column', gap: '1rem', padding: '1rem' }}>
@@ -94,6 +113,63 @@ export default function AddPost() {
                     </div>
                 </div>
 
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '.4rem', width: '100%', position: 'relative', background: 'white', padding: '1rem', borderRadius: '1rem' }}>
+
+
+                    {
+                        tag.length !== 0 &&
+                        <>
+                            <span>Tags</span>
+                            <div style={{ display: 'flex', gap: '.5rem', overflowX: 'auto', background: '#f5f5f5', padding: '.6rem', borderRadius: '.5rem' }}>
+                                {
+                                    tag.map((tg, idx) => (
+                                        <div key={idx} style={{ background: '#ffffff', padding: '.5rem', flex: '0 0 auto', borderRadius: '.5rem' }}>
+                                            <span>{tg}</span>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    }
+                    <span>Add Tags</span>
+                    <ul style={{ display: 'flex', background: '#f5f5f5', overflowX: 'auto', listStyle: 'none', gap: '.5rem', position: 'relative', padding: '.5rem', scrollbarColor: 'transparent', scrollbarWidth: 'none' }}>
+
+                        {
+                            !addTagPanel &&
+                            <>
+                                <button onClick={(e) => handTagPanel(true, e)} style={{ background: '#010a1b', display: 'flex', border: 'none', flexDirection: 'column', alignItems: 'center', position: 'sticky', left: '0', justifyContent: 'center', boxShadow: '0 0 .2rem #dfdddde7', flex: '0 0 auto', width: '40px', height: '30px', borderRadius: '.5rem' }}>
+                                    <img src="https://img.icons8.com/?size=100&id=oF7jXJoq6nNX&format=png&color=ffffff" width={"20"} height={"20"} />
+                                </button>
+
+                                {
+                                    categories.map((cat, idx) => (
+                                        <li onClick={() => handleTag(cat)} key={idx} style={{ backgroundColor: tag.includes(cat) ? '#1499ec40' : 'white', flex: '0 0 auto', padding: '.2rem 1rem', borderRadius: '1rem' }}>{cat}</li>
+                                    ))
+                                }
+                            </>
+                        }
+
+                        {
+                            addTagPanel &&
+                            <div style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '.5rem', borderRadius: '1rem', background: '#f5f5f5' }}>
+                                <input ref={tagInputElement} style={{ padding: '.5rem', fontSize: '1rem', flex: '1', border: 'none', outline: 'none', background: 'none' }} placeholder="Tag name" />
+                                <div style={{ display: 'flex', gap: '.5rem', alignItems: 'center' }}>
+
+                                    <button onClick={(e) => handTagPanel(false, e)} type="button" style={{ marginInlineStart: 'auto', background: '#f5f5f5', border: 'none', color: 'black', padding: '.5rem', borderRadius: '.5rem' }}>
+                                        <span>Cancel</span>
+                                    </button>
+
+                                    <button onClick={(e) => { handTagPanel(false, e); handleAddTag() }} type="button" style={{ marginInlineStart: 'auto', background: '#010a1b', border: 'none', color: 'white', padding: '.5rem', borderRadius: '.5rem' }}>
+                                        <span>Add</span>
+                                    </button>
+                                </div>
+                            </div>
+                        }
+                    </ul>
+
+
+                </div>
+
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '.3rem', width: '100%' }}>
                     <label htmlFor="prev-link" >Preview Link</label>
                     <input id="prev-link" placeholder="https://myapp.com" style={{ width: '100%', padding: '1rem', outline: 'none', border: 'none', borderRadius: '.5rem', fontSize: '1rem' }} />
@@ -111,7 +187,7 @@ export default function AddPost() {
                         </select>
                     </div>
                 </div>
-                <button style={{ width: '100%', padding: '1rem', borderRadius: '1rem', marginBlockStart: '6rem', background: '#010a1b', border: 'none', color: 'white' }}>Post</button>
+                <button style={{ width: '100%', padding: '1rem', borderRadius: '1rem', marginBlockStart: '4rem', marginBlockEnd: '1rem', background: '#010a1b', border: 'none', color: 'white' }}>Post</button>
             </form>
         </div>
     )
